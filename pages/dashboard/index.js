b1cab2ef954bc509c1b38b6044c1247f21ba8087
Malhar <malhardhaygude@gmail.com>
2023-02-01 23:46:17 +0530

    First Change ..

---
+++ pages/dashboard/index.js
@@ -0,0 +1,14 @@
+import Header from "../../components/Header";
+import styles from "../../styles/Dashboard.module.scss";
+
+export default function Dashboard() {
+    return (
+      <div className={styles.page}>
+        <Header />
+        <div className={styles.container}>
+          {/* write your code here */}
+          my name is chutyaaaa
+        </div>
+      </div>
+    );
+  }
--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,13 @@
 import Header from "../components/Header";
 import styles from "../styles/Home.module.scss";
-import LeftPanel from "../components/home/LeftPanel";
-import RightPanel from "../components/home/Content";
-import Doughnut from "../components/home/Doughnut";
-import Fibrosis from "../components/home/Fibrosis";
-import Title from "../components/home/Title";
+
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <Header />
       <div className={styles.container}>
-        <div className={styles.upper}>
-          <div className={styles.bg}></div>
-          {/* <Doughnut /> */}
-          <Title />
-        </div>
+        {/* write your code here */}
       </div>
     </div>
   );

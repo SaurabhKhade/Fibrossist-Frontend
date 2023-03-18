937dc5455b0900acf92b5946d42fb42e3b3d9763
SaurabhKhade <khadesaurabh056@gmail.com>
2023-03-18 16:33:23 +0530

    bug fixed

--- host.js
+++ host.js
@@ -2,5 +2,5 @@ export default function getHost() {
   if (process.env.NODE_ENV === "development") {
     return "http://localhost:5000";
   }
-  return "http://fibrossist-connector.vercel.app";
+  return "https://fibrossist-connector.vercel.app";
 }
